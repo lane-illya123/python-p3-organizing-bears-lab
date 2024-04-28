22025f0261c752519c73e445cf5a0697b52fccda
illyalane <lane_illya@yahoo.com>
2024-04-28 17:23:12 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Mr. Chocolate', 4, 'M', 'black', 'calm' , 0);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Rowdy', 10, 'M', 'red', 'moody' , 1);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Tabitha', 6, 'F', 'white', 'calm' , 1);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Sergeant Brown', 20, 'M', 'white', 'moody' , 1);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Melissa', 10, 'F', 'black', 'calm' , 0);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Grinch', 10, 'F', 'black', 'moody' , 1);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+('Wendy', 15, 'F', 'red', 'moody' , 0);
+
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES 
+(NULL, NULL, NULL, NULL, NULL , NULL);
